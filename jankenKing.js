d1381bf79664bf238eb1f2f4370a9b5d781adcb6
yamatairiku-dev <yamatairiku.dev@gmail.com>
2022-08-06 21:06:16 +0900

    初版

---
+++ jankenKing.js
@@ -0,0 +1,36 @@
+
+(() => {
+  'use strict';
+
+  kintone.events.on('app.record.detail.show', (event) => {
+    const myHeaderMenuSpace = kintone.app.record.getHeaderMenuSpaceElement();
+    const startButton = document.createElement('button');
+    startButton.id = 'janken_start_button';
+    startButton.innerText = 'ジャンケンキングに挑戦！！';
+    myHeaderMenuSpace.innerText = '';
+
+    const jankenItems = ['グー', 'チョキ', 'パー'];
+    // ジャンケンキングに挑戦！！ボタンクリック
+    startButton.onclick = () => {
+      // ジャンケンスペース
+      const jankenSpace = kintone.app.record.getSpaceElement('my_space_field');
+      jankenSpace.innerText = '最初はグー！！\nジャンケン・・・\n';
+      jankenSpace.style.width = '400px';
+      jankenSpace.parentNode.style.width = '400px';
+
+      for (let i = 0; i < jankenItems.length; i++) {
+        const tmpButton = document.createElement('button');
+        tmpButton.id = 'j_button' + i;
+        tmpButton.innerText = jankenItems[i] + '！！';
+        tmpButton.onclick = (e) => {
+          const you = e.target.id.replace('j_button', '');
+          const com = (you + 2) % jankenItems.length;
+          jankenSpace.innerText = 'あなた：' + jankenItems[you] + '\nジャンケンキング:' + jankenItems[com] + '\nもう一度やりますか？';
+        };
+        jankenSpace.appendChild(tmpButton);
+      }
+    };
+    myHeaderMenuSpace.appendChild(startButton);
+    return event;
+  });
+})();
