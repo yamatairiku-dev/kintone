a1c6b9968565b4e748aa9516d1847ff5a8b2c261
yamatairiku-dev <yamatairiku.dev@gmail.com>
2022-08-07 18:39:21 +0900

    一旦コミット

--- jankenKing.js
+++ jankenKing.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  kintone.events.on('app.record.detail.show', (event) => {
+  kintone.events.on('app.record.detail.show', event => {
     const myHeaderMenuSpace = kintone.app.record.getHeaderMenuSpaceElement();
     const startButton = document.createElement('button');
     startButton.id = 'janken_start_button';
@@ -19,13 +19,13 @@
       jankenSpace.style.width = '400px';
       jankenSpace.parentNode.style.width = '400px';
 
-      const buttonColor = ['btn btn-primary','btn btn-danger','btn btn-warning'];
+      const buttonColor = ['btn btn-primary', 'btn btn-danger', 'btn btn-warning'];
       for (let i = 0; i < jankenItems.length; i++) {
         const tmpButton = document.createElement('button');
         tmpButton.id = 'j_button' + i;
         tmpButton.className = buttonColor[i];
         tmpButton.innerText = jankenItems[i] + '！！';
-        tmpButton.onclick = (e) => {
+        tmpButton.onclick = e => {
           const you = e.target.id.replace('j_button', '');
           const com = (you + 2) % jankenItems.length;
           jankenSpace.innerText = 'あなた：' + jankenItems[you] + '\nジャンケンキング:' + jankenItems[com] + '\nもう一度やりますか？';
